64dd05b99a35c9b0ef6c9d25c25403e063758169
Tristan Leo <mail@tristan-leo.de>
2011-11-27 19:28:35 +0100

    update on connect

--- hub.js
+++ hub.js
@@ -1,19 +1,23 @@
 var io = require('socket.io').listen(1337);
 var _ = require('underscore');
 
+var latest = "";
+
 io.sockets.on('connection', function (socket) {
   socket.on('set nickname', function (name) {
     socket.set('nickname', name, function () {
       socket.broadcast.emit('user connected',name);
       socket.emit('ready');
+      socket.broadcast.emit('update revision',latest);
     });
   });
 
   socket.on('push revision', function (data) {
     socket.get('nickname', function (err, name) {
       console.log('revision by ', name);
+      latest = data;
       console.log(data);
-      socket.broadcast.emit('update revision',data);
+      socket.broadcast.emit('update revision',latest);
     });
   });
 });
